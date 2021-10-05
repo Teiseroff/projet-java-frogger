33f1de2bae3fa99af36314b7fe2e0c7c24cf0148
Teiseroff <hendrik.hiddinga@gmail.com>
2021-10-05 20:58:44 +0200

    App initale
    une blague

--- src/App.java
+++ src/App.java
@@ -1,5 +1,39 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+import javafx.application.Application ;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+
+public class App extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        Button btn = new Button() ;
+        btn.setText("START") ;
+        btn.setOnAction(new EventHandler<ActionEvent>(){
+            @Override
+            public void handle(ActionEvent event){
+                System.out.println("Start !") ;
+            }
+        });
+
+        StackPane root = new StackPane() ;
+        root.getChildren().add(btn) ;
+
+        Scene scene = new Scene(root, 300, 250) ;
+        primaryStage.setTitle("Crazy Frog");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+
     }
+
+    public static void main(String[] args) {
+        launch(args) ;
+    }
+
 }
