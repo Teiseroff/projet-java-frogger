738fc4e65e7eceb1078f96c2f72dfad78b04eef2
Teiseroff <hendrik.hiddinga@gmail.com>
2021-10-19 18:48:51 +0200

    c'est bleuuuu

--- src/java/com/frogger/App.java
+++ src/java/com/frogger/App.java
@@ -5,6 +5,7 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TitledPane;
@@ -19,7 +20,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        StackPane root = new StackPane() ;
+        Group root = new Group() ;
 
         Button btn = new Button() ;
         btn.setText("START") ;
@@ -30,27 +31,36 @@ public class App extends Application {
             }
         });
 
-        //root.getChildren().add(btn) ;
-
         Text crazyfrog_title = new Text();
         crazyfrog_title.setText("CRAZY FROG");
 
-        GridPane menuGrid = new GridPane() ;
-        menuGrid.setMinSize(400,200);
-        menuGrid.setPadding(new Insets(10, 10, 10, 10));
-        menuGrid.setVgap(40);
-        menuGrid.setHgap(5);
-        menuGrid.setAlignment(Pos.CENTER);
-        menuGrid.add(crazyfrog_title, 0, 0);
-        menuGrid.add(btn, 0, 1);
-        btn.getStylesheets().add("mainstyle.css") ;
+        crazyfrog_title.setX(50) ;
+        crazyfrog_title.setY(50) ;
+        btn.setLayoutX(70);
+        btn.setLayoutY(50); 
 
-        Scene titleMenu = new Scene(menuGrid, 500, 400) ;
-        primaryStage.setTitle("Crazy Frog");
-        primaryStage.setScene(titleMenu);
+        root.getChildren().add(btn) ;
+        root.getChildren().add(crazyfrog_title);
+
+
+//        GridPane menuGrid = new GridPane();
+//        menuGrid.setMinSize(400,200);
+//        menuGrid.setPadding(new Insets(10, 10, 10, 10));
+//        menuGrid.setVgap(40);
+//        menuGrid.setHgap(5);
+//        menuGrid.setAlignment(Pos.CENTER);
+//        menuGrid.add(crazyfrog_title, 0, 0);
+//        menuGrid.add(btn, 0, 1);
+//        btn.getStylesheets().add("mainstyle.css") ;
+
+        Scene titleMenu = new Scene(root, 500, 400, Color.LIGHTSKYBLUE) ;
+        Stage stage = new Stage() ;
+
+        stage.setTitle("Crazy Frog");
+        stage.setScene(titleMenu);
         //Image window_icon = new Image("Users/hendr/Desktop/COURS/S3/JAVA/projet-java-frogger/src/resources/assets/shrek.png") ;
-        //primaryStage.getIcons().add(window_icon) ;
-        primaryStage.show();
+        //stage.getIcons().add(window_icon) ;
+        stage.show();
     }
 
     public void mainstart(Stage primaryStage) {
@@ -60,7 +70,7 @@ public class App extends Application {
 
 
         primaryStage.setScene(mainMenu);
-        primaryStage.centerOnScreen();
+        primaryStage.setFullScreen(true);
         primaryStage.show();
 
 
